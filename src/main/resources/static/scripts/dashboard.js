2594d65eca8f24b9a46efe0c6f4f8db5c0ef5c0e
Dana Kutálková <kutalkovad@gmail.com>
2019-05-13 11:46:27 +0200

    adjust .js to the current version of dashboard

--- src/main/resources/static/scripts/dashboard.js
+++ src/main/resources/static/scripts/dashboard.js
@@ -1,26 +1,41 @@
 let rowTouched;
-let rowClicked;
-let detailRow;
-
 
 function highlightRow(event) {
     rowTouched = event.target;
-    detailRow = rowTouched.nextElementSibling;
-
     rowTouched.classList.add("highlighted");
-    detailRow.classList.add("highlighted");
 }
 
 function revertHighlightRow(event) {
     rowTouched.classList.remove("highlighted");
-    detailRow.classList.remove("highlighted");
 }
 
 function showRowDetail(event) {
     //only a cell is clicked upon, so we need its parent in order the get a whole row
-    rowClicked = event.target.parentNode;
-    detailRow = rowClicked.nextElementSibling;
+    let rowClicked = event.target.parentNode;
+    rowClicked.classList.toggle("row_open");
 
-    detailRow.classList.toggle("row_opened");
+    let detailRow1 = rowClicked.nextElementSibling;
+    console.log(detailRow1);
+    let detailRow2 = detailRow1.nextElementSibling;
+    console.log(detailRow2);
+    let detailRow3 = detailRow2.nextElementSibling;
+    console.log(detailRow3);
+
+    if (rowClicked.classList.contains("row_open")) {
+        detailRow1.classList.remove("row_closed");
+        detailRow2.classList.remove("row_closed");
+        detailRow3.classList.remove("row_closed");
+        detailRow1.classList.add("row_open");
+        detailRow2.classList.add("row_open");
+        detailRow3.classList.add("row_open");
+    } else {
+        detailRow1.classList.remove("row_open");
+        detailRow2.classList.remove("row_open");
+        detailRow3.classList.remove("row_open");
+        detailRow1.classList.add("row_closed");
+        detailRow2.classList.add("row_closed");
+        detailRow3.classList.add("row_closed");
+    }
+
 }
 
